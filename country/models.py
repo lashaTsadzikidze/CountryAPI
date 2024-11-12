0eed07daa5827aa52681c88482cade594face29e
lasha1222 <lasha.tsadzikidze@geolab.edu.ge>
2024-11-12 19:28:32 +0400

    create country model and make migrations

--- country/models.py
+++ country/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=250)
+    capital_city = models.CharField(max_length=100)
+    population = models.IntegerField()
+    area = models.FloatField()
+    official_languages = models.CharField(max_length=300)
+    continent = models.CharField(max_length=100)
+    main_cities = models.TextField()
+    climate = models.FloatField()
+    currency = models.CharField(max_length=20)
+    gdp = models.FloatField()
+
+    def __str__(self):
+        return self.name
